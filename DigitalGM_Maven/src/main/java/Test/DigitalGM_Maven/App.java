18fd508733e61e1481ce622470221241c60eb821
24024 <24024@BLRDRIGWN24024.ITCINFOTECH.com>
2020-03-04 10:49:18 +0530

    First Commit

---
+++ DigitalGM_Maven/src/main/java/Test/DigitalGM_Maven/App.java
@@ -0,0 +1,13 @@
+package Test.DigitalGM_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
